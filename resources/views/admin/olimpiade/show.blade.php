1cde5f585a13fa86f0c3b4aadac9629b9353a600
punkestu <pangestubima89@gmail.com>
2024-10-23 15:18:01 +0700

    delete feedback

--- resources/views/admin/olimpiade/show.blade.php
+++ resources/views/admin/olimpiade/show.blade.php
@@ -108,7 +108,9 @@
                                                 d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z" />
                                         </svg>
                                         Edit</button>
-                                    <a href="{{ route('question.delete', $question->id) }}"
+                                    <button data-modal-target="confirmation-modal"
+                                        data-modal-toggle="confirmation-modal"
+                                        onclick="setDeleteUrl({{ $question->id }})"
                                         class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 flex items-center">
                                         <svg class="w-5 h-5 text-white dark:text-gray-800" aria-hidden="true"
                                             xmlns="http://www.w3.org/2000/svg" width="24" height="24"
@@ -117,7 +119,7 @@
                                                 stroke-linejoin="round" stroke-width="2"
                                                 d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
                                         </svg>
-                                        Hapus</a>
+                                        Hapus</button>
                                 </td>
                             </tr>
                         @endforeach
@@ -135,6 +137,42 @@
         </div>
     </main>
 
+    <div id="confirmation-modal" tabindex="-1"
+        class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div class="relative p-4 w-full max-w-md max-h-full">
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <button type="button"
+                    class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                    data-modal-hide="confirmation-modal">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                    </svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+                <div class="p-4 md:p-5 text-center">
+                    <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                    <h3 class="text-lg font-normal text-gray-500 dark:text-gray-400">Yakin ingin hapus soal ini?
+                    </h3>
+                    <p class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">(tidak bisa dikembalikan)
+                    </p>
+                    <a
+                        class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                        Tetap Hapus
+                    </a>
+                    <button data-modal-hide="confirmation-modal" type="button"
+                        class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Batal</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
     @include('admin.olimpiade.question.create', ['olimpiade_id' => $olimpiade->id])
     @if (count($olimpiade->questions) > 0)
         @include('admin.olimpiade.question.show')
@@ -142,6 +180,11 @@
     @endif
     @include('components.errors')
     <script>
+        function setDeleteUrl(id) {
+            const confirmationModal = document.getElementById('confirmation-modal');
+            confirmationModal.querySelector('a').href = `/question/${id}/delete`;
+        }
+
         function setQuestion(index, data) {
             const showModal = document.getElementById('show-modal');
             const editModal = document.getElementById('edit-modal');
--- resources/views/admin/participant/index.blade.php
+++ resources/views/admin/participant/index.blade.php
@@ -101,7 +101,9 @@
                                                 d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z" />
                                         </svg>
                                         Ganti Password</a>
-                                    <a href="{{ route('participant.delete', $participant->id) }}"
+                                    <button data-modal-target="confirmation-modal"
+                                        data-modal-toggle="confirmation-modal"
+                                        onclick="setDeleteUrl({{ $participant->id }})"
                                         class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900  flex items-center">
                                         <svg class="w-5 h-5 text-white dark:text-gray-800" aria-hidden="true"
                                             xmlns="http://www.w3.org/2000/svg" width="24" height="24"
@@ -111,7 +113,7 @@
                                                 d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
                                         </svg>
                                         Hapus
-                                    </a>
+                                    </button>
                                 </td>
                             </tr>
                         @endforeach
@@ -169,4 +171,44 @@
             </div>
         </div>
     </div>
+
+    <div id="confirmation-modal" tabindex="-1"
+        class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div class="relative p-4 w-full max-w-md max-h-full">
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <button type="button"
+                    class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                    data-modal-hide="confirmation-modal">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                    </svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+                <div class="p-4 md:p-5 text-center">
+                    <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                    <h3 class="text-lg font-normal text-gray-500 dark:text-gray-400">Yakin ingin hapus peserta ini?
+                    </h3>
+                    <p class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">(tidak bisa dikembalikan)
+                    </p>
+                    <a
+                        class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                        Tetap Hapus
+                    </a>
+                    <button data-modal-hide="confirmation-modal" type="button"
+                        class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Batal</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script>
+        function setDeleteUrl(id) {
+            document.querySelector('#confirmation-modal a').href = `/participant/${id}/delete`;
+        }
+    </script>
 </x-layout>
