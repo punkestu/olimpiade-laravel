52bab919aa60c8f40ca8fb3362b236f012a32a69
punkestu <pangestubima89@gmail.com>
2024-11-01 13:31:01 +0700

    optimize

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,8 @@
 import "./bootstrap";
 import "flowbite";
-import { initRichText } from "./richtext";
-import { monitor } from "./monitor";
-import { initFlatpickr } from "./flatpickr";
 
 document.addEventListener("DOMContentLoaded", () => {
-    initRichText();
-    monitor();
-    initFlatpickr();
+    import("./richtext").then(({ initRichText }) => initRichText());
+    import("./monitor").then(({ monitor }) => monitor());
+    import("./flatpickr").then(({ initFlatpickr }) => initFlatpickr());
 });
