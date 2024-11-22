3361c332ef3fba97d5fe9bed527160e39fb7520b
punkestu <pangestubima89@gmail.com>
2024-11-22 21:28:11 +0700

    update point export to xslx

--- app/Http/Controllers/PointExportController.php
+++ app/Http/Controllers/PointExportController.php
@@ -2,30 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Exports\PointExport;
 use App\Models\Olimpiade;
 use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
 
 class PointExportController extends Controller
 {
-    public function export($id) {
+    public function export($id)
+    {
         $olimpiade = Olimpiade::find($id);
         $participants = $olimpiade->participants;
-
-        $filename = 'tmp/Point ' . $olimpiade->name . '.csv';
-        $handle = fopen($filename, 'w+');
-
-        fputcsv($handle, ['Nama', 'Asal Sekolah', 'Kelas', 'Nilai']);
-
+        $filename = 'Point ' . $olimpiade->name . '.xlsx';
+        $points = [[
+            'Nama',
+            'Asal Sekolah',
+            'Kelas',
+            'Poin'
+        ]];
         foreach ($participants as $participant) {
-            fputcsv($handle, [$participant->name, $participant->asal_sekolah, $participant->kelas, $participant->point->sum('poin') - $participant->minusPoint->count()]);
+            $points[] = [$participant->name, $participant->asal_sekolah, $participant->kelas, $participant->point->count() > 0 ? $participant->point->sum('poin') - $participant->minusPoint->count() : "0"];
         }
 
-        fclose($handle);
-
-        $headers = [
-            'Content-Type' => 'text/csv',
-        ];
-
-        return response()->download($filename, 'Point ' . $olimpiade->name . '.csv', $headers);
+        return Excel::download(new PointExport($points), $filename);
     }
 }
